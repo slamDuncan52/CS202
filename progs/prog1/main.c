09588861bff3f0f9ac82a5eb6c93f9abe6c37f66
slamDuncan52 <dunc0474@kettering.edu>
2015-01-17 11:48:08 -0500

    First program outline

---
+++ progs/prog1/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#DEFINE some_num 1000
+
+int main(int argc char** argv){
+/*Get input*/
+	for(i=0;i<userInput;i++){
+/*Test if palindrome*/
+		/*if palindrome*/
+		/*convert to binary*/
+		/*test if binary palindrome*/
+		/*if binary palindrome*/
+			/*print num and binary num*/
+			/*add to running total*/
+	
+	}
+	return 0;
+}
+
+int testPalindrome(int input){
+/*convert to array of chars*/
+	/*find length of array*/
+	/*begin testing first against last for equality*/
+	/*continue, moving inwards*/
+	/*return results*/
+}
+
+int convertToBinary(int input){
+/*find first power of 2 greater than input
+}
