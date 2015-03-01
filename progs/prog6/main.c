c946fef793c8caa3ba1dd9af6d760a6ed265743c
Mitch Duncan <dunc0474@kettering.edu>
2015-03-01 14:27:55 -0500

    Program 6 mostly fxnl. Some weirdness on very long input.....

---
+++ progs/prog6/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BLOCKSIZE 1000
+
+int appendFlag = 0;
+
+char * stdInGrab();
+
+int main(int argc, char *argv[]){
+	/* Handle Args */
+	int argCount = 1;
+	int fileCount = -1;
+	char *fileList[argc];
+	while(argCount < argc){
+		if(!strcmp(argv[argCount], "-a")){
+			appendFlag = 1;
+		} else if(!strcmp(argv[argCount], "-i")){
+			signal(SIGINT,SIG_IGN);
+		} else {
+			fileList[++fileCount] = argv[argCount];
+		}
+		argCount++;
+	}
+	/* Grab all the input */
+	char *inputString = stdInGrab();
+
+	/* Write to each file */
+	FILE *currentFile;
+	while(fileCount >= 0){
+		if(appendFlag){
+			currentFile = fopen(fileList[fileCount--], "a");
+		} else {
+			currentFile = fopen(fileList[fileCount--],"w");
+		}	
+		fputs(inputString,currentFile);
+		close(currentFile);
+	}
+	return 0;
+}
+
+char * stdInGrab(){
+	int numOfLines = 1;
+	char *returnString;
+	char *sizeSwap;
+	char lineBuffer[BLOCKSIZE];
+
+	sizeSwap = malloc(numOfLines * sizeof(char) * BLOCKSIZE);
+	returnString = malloc(numOfLines * sizeof(char) * BLOCKSIZE);
+	while(fgets(lineBuffer, BLOCKSIZE, stdin) != NULL){
+		if((strlen(lineBuffer) + strlen(returnString)) > (BLOCKSIZE * numOfLines)){
+			strcpy(sizeSwap, returnString);
+			returnString = malloc(numOfLines* sizeof(char) * BLOCKSIZE);
+			strcpy(returnString, sizeSwap);
+			sizeSwap = malloc(numOfLines * sizeof(char) * BLOCKSIZE);
+		}
+		strcat(returnString, lineBuffer);
+		numOfLines++;
+	}
+	printf("%s",returnString);
+	return returnString;
+}
